c1ef8079bdafbf9d74b68c86a4b8f3e8c836f5b0
Escuela virtual selca <escuelavirtualselca@gmail.com>
2019-11-07 13:08:25 -0300

    se vinculo el modelo persona con localidad y se creo el metodo mostrar

--- app/Persona.php
+++ app/Persona.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Persona extends Model
 {
-    //
+    public function localidad()
+    {
+        return $this->belongsTo('App\Localidad');
+    }
+
+    public function mostrar(){
+        $localidad = empty($this->localidad)? "Sin datos": $this->localidad->nombre . " - " . $this->localidad->departamento->nombre;
+        return number_format($this->dni, 0, ',', '.') . " - " . $this->nombre . " " . $this->apellido . " - " . $localidad;
+    }
 }
