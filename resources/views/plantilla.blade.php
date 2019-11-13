0ae4acdfc2d9c17dc6a457b1977e8c0cf2c3ace2
Escuela virtual selca <escuelavirtualselca@gmail.com>
2019-11-13 08:03:43 -0300

    se agregaron de forma local los archivos css y js

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -6,27 +6,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
         <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-
-        <style>
-            .navbar-nav li:hover > ul.dropdown-menu {
-                display: block;
-            }
-            .dropdown-submenu {
-                position:relative;
-            }
-            .dropdown-submenu>.dropdown-menu {
-                top:0;
-                left:100%;
-                margin-top:-6px;
-            }
-            /* rotate caret on hover */
-            .dropdown-menu > li > a:hover:after {
-                text-decoration: underline;
-                transform: rotate(-90deg);
-            } 
-        </style>
+        <link rel="stylesheet" href="{{ url ('bootstrap/css/bootstrap.css') }}">
+        <link rel="stylesheet" href="{{ url ('jquery/css/jquery-ui.css') }}">
+        <link rel="stylesheet" href="{{ url ('css/submenu.css') }}">
 
         <title>Salutem</title>
     </head>
@@ -67,10 +49,10 @@
 
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script src="{{ url('jquery/js/jquery-3.3.1.js') }}"></script>
+        <script src="{{ url('jquery/js/jquery-ui.js') }}" ></script>
+        <script src="{{ url('bootstrap/js/bootstrap.js') }}"></script>
+        <script src="{{ url('bootstrap/js/popper.js') }}"></script>
         @yield("script")
 
     </body> 
